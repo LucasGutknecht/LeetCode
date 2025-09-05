96dd00805d3f680a4c1d0fb0478de6d99651775b
Lucas <lucas@albesbrasil.com.br>
2025-09-05 20:18:52 -0300

    Update code.

--- 10_regularExpressionMatching.c
+++ 10_regularExpressionMatching.c
@@ -1,37 +1,47 @@
 #include <regex.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 bool isMatch(char* s, char* p) {
-
     regex_t reegex;
-
-    if ((!strchr(p, '*') && !strchr(p, '.')) && (s != p)) {
+    if ((!strchr(p, '*') && !strchr(p, '.')) && (strcmp(s, p) != 0)) {
         return false;
     }
-
     size_t length = strlen(p);
     int value;
 
-    for(int i = 0; i < length; i++){
-        if(p[i] == '*' && i == 0){
+    char* modified_p = malloc(length + 3);
+    strcpy(modified_p, "^");
+    strcat(modified_p, p);
+    strcat(modified_p, "$");
+
+    for(int i = 1; i < length + 1; i++){
+        if(modified_p[i] == '*' && i == 1){
+            free(modified_p);
             return false;
-        } else if (p[i] == '*'){
-            p[i] = 0;
-        } else if (p[i] == '.'){
-            p[i] = '.';
+        } else if (modified_p[i] == '*'){
+            modified_p[i] = '*';
+        } else if (modified_p[i] == '.'){
+            modified_p[i] = '.';
         } else {
-            p[i] = p[i];
+            modified_p[i] = modified_p[i];
         }
     }
 
-    if(strchr(p, '*') == NULL){
-      value = regcomp( &reegex, p, 0);
-    }else{
-      value = regcomp( &reegex, p, REG_EXTENDED);
+    if(strchr(modified_p, '*') == NULL){
+        value = regcomp(&reegex, modified_p, 0);
+    } else {
+        value = regcomp(&reegex, modified_p, REG_EXTENDED);
     }
 
-    value= regexec( &reegex, s, 0, NULL, 0);
+    if(value != 0) {
+        free(modified_p);
+        return false;
+    }
 
+    value = regexec(&reegex, s, 0, NULL, 0);
+    regfree(&reegex);
+    free(modified_p);
     return (value == 0) ? true : false;
 }
